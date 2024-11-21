d2b3c0fd2da2e2bad117d16519807253506afed1
Jopvdb <jopvdbrand@gmail.com>
2024-11-21 17:15:52 +0100

    cyth

--- utils.pyx
+++ utils.pyx
@@ -2,7 +2,7 @@
 Copyright (C) 2016 Yaniv Erlich
 License: GPLv3-or-later. See COPYING file for details.
 """
-str.maketrans
+from str import maketrans
 import struct
 import random
 import os
@@ -12,8 +12,8 @@ import argparse
 intab = "0123"
 outtab = "ACGT"
 
-trantab = str.maketrans(intab, outtab)
-revtab = str.maketrans(outtab, intab)
+trantab = maketrans(intab, outtab)
+revtab = maketrans(outtab, intab)
 
 
 def charN(str, N):
